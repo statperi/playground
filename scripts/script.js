9f522e21cbb14ed6f9875df119e7d2f8de731261
Periklis <periklis.stathopoulos@voicesage.com>
2021-11-01 17:56:57 +0000

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 window.onload = () => {
 
     document.querySelector('button[data-action="attack"]').addEventListener('click', function () {
+        console.log('attack');
         animate('clip: Skeleton_Attak; loop: once; duration:1');
     });
 
     document.querySelector('button[data-action="attack"]').addEventListener('click', function () {
+        console.log('die');
         animate('clip: Skeleton_Death; loop: once; duration:1; clampWhenFinished:true;');
     });
 };
