785fe4073ac3293565497e79ae6ed55f90deec24
Periklis <periklis.stathopoulos@voicesage.com>
2021-11-02 16:32:06 +0000

    try the bear once again

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,21 @@ window.onload = () => {
 
     document.querySelector('button[data-action="skeleton"]').addEventListener('click', function () {
         var skeleton = $('.skeleton')[0];
-        var skeleton2 = $('.skeleton2')[0];
-        
-        // var bear = $('.bear')[0];
+        // var skeleton2 = $('.skeleton2')[0];
+        var bear = $('.bear')[0];
 
         animate(skeleton, 'clip: Skeleton_Attak; loop: once; duration:3; clampWhenFinished: true;');
-        animate(skeleton2, 'clip: Skeleton_Death; loop: once; duration:3; clampWhenFinished: true;');
-        // animate(bear, 'clip: Arm_Bear|Death_1; loop: once; duration:1');
+        // animate(skeleton2, 'clip: Skeleton_Death; loop: once; duration:3; clampWhenFinished: true;');
+        animate(bear, 'clip: Arm_Bear|Death_1; loop: once; duration:3; clampWhenFinished: true;');
     });
 
     document.querySelector('button[data-action="bear"]').addEventListener('click', function () {
         var skeleton = $('.skeleton')[0];
-        var skeleton2 = $('.skeleton2')[0];
-        // var bear = $('.bear')[0];
-        // animate(bear, 'clip: Arm_Bear|attack_1; loop: once; duration:1');
-        animate(skeleton2, 'clip: Skeleton_Attak; loop: once; duration:3; clampWhenFinished: true;');
+        // var skeleton2 = $('.skeleton2')[0];
+        var bear = $('.bear')[0];
+
+        // animate(skeleton2, 'clip: Skeleton_Attak; loop: once; duration:3; clampWhenFinished: true;');
+        animate(bear, 'clip: Arm_Bear|attack_1; loop: once; duration:3; clampWhenFinished: true;');
         animate(skeleton, 'clip: Skeleton_Death; loop: once; duration:3; clampWhenFinished: true;');
     });
 };
