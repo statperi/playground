9813bd70acd6a9694a9c69b9c36e7d1514a48e9d
Periklis <periklis.stathopoulos@voicesage.com>
2021-11-02 16:24:19 +0000

    test

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,8 @@ window.onload = () => {
         
         // var bear = $('.bear')[0];
 
-        animate(skeleton, 'clip: Skeleton_Attak; loop: once; duration:1');
-        animate(skeleton2, 'clip: Skeleton_Death; loop: once; duration:1;');
+        animate(skeleton, 'clip: Skeleton_Attak; loop: once; duration:3; clampWhenFinished: true;');
+        animate(skeleton2, 'clip: Skeleton_Death; loop: once; duration:3; clampWhenFinished: true;');
         // animate(bear, 'clip: Arm_Bear|Death_1; loop: once; duration:1');
     });
 
@@ -16,8 +16,8 @@ window.onload = () => {
         var skeleton2 = $('.skeleton2')[0];
         // var bear = $('.bear')[0];
         // animate(bear, 'clip: Arm_Bear|attack_1; loop: once; duration:1');
-        animate(skeleton2, 'clip: Skeleton_Attak; loop: once; duration:1');
-        animate(skeleton, 'clip: Skeleton_Death; loop: once; duration:1;');
+        animate(skeleton2, 'clip: Skeleton_Attak; loop: once; duration:3; clampWhenFinished: true;');
+        animate(skeleton, 'clip: Skeleton_Death; loop: once; duration:3; clampWhenFinished: true;');
     });
 };
 
@@ -25,7 +25,7 @@ window.onload = () => {
 function animate(model, action) {
     // var skeleton = $('.skeleton')[0];
     model.setAttribute('animation-mixer', action);
-    //model.removeAttribute('animation-mixer');
+    // model.removeAttribute('animation-mixer');
 }
 
 
