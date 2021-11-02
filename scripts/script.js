c25457106f3cded7e3c569a9894c66850002378d
Periklis <periklis.stathopoulos@voicesage.com>
2021-11-02 16:12:46 +0000

    skeletons

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,22 @@
 window.onload = () => {
 
     document.querySelector('button[data-action="skeleton"]').addEventListener('click', function () {
-        console.log('skeleton');
-
         var skeleton = $('.skeleton')[0];
-        var bear = $('.bear')[0];
+        var skeleton2 = $('.skeleton2')[0];
+        
+        // var bear = $('.bear')[0];
 
         animate(skeleton, 'clip: Skeleton_Attak; loop: once; duration:1');
-        animate(bear, 'clip: Arm_Bear|Death_1; loop: once; duration:1');
+        animate(skeleton2, 'clip: Skeleton_Death; loop: once; duration:1;');
+        // animate(bear, 'clip: Arm_Bear|Death_1; loop: once; duration:1');
     });
 
     document.querySelector('button[data-action="bear"]').addEventListener('click', function () {
-        console.log('bear');
-
         var skeleton = $('.skeleton')[0];
-        animate(bear, 'clip: Arm_Bear|attack_1; loop: once; duration:1');
+        var skeleton2 = $('.skeleton2')[0];
+        // var bear = $('.bear')[0];
+        // animate(bear, 'clip: Arm_Bear|attack_1; loop: once; duration:1');
+        animate(skeleton2, 'clip: Skeleton_Attak; loop: once; duration:1');
         animate(skeleton, 'clip: Skeleton_Death; loop: once; duration:1;');
     });
 };
