d536d2e67b9d757f9a0e5f81ee57bcc97dd16487
Periklis <periklis.stathopoulos@voicesage.com>
2021-12-13 11:24:18 +0000

    test

--- scripts/gesture-handler.js
+++ scripts/gesture-handler.js
@@ -42,10 +42,10 @@ AFRAME.registerComponent("gesture-handler", {
         console.log('visible: ', this.isVisible);
 
          if (this.isVisible) {
-            this.el.object3D.rotation.y +=
-                event.detail.positionChange.x * this.data.rotationFactor;
-            //this.el.object3D.rotation.x +=
-            //    event.detail.positionChange.y * this.data.rotationFactor;
+            //this.el.object3D.rotation.y +=
+            //    event.detail.positionChange.x * this.data.rotationFactor;
+            this.el.object3D.rotation.x +=
+                event.detail.positionChange.y * this.data.rotationFactor;
          }
     },
 
